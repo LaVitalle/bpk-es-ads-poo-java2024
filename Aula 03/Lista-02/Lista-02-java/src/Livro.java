4bfb96282e5ff87f10cd3078e406bc1d1b6451b7
LaVitalle <vitorhugoinacio@outlook.com>
2024-08-16 21:20:53 -0300

    UP-VITOR

---
+++ Aula 03/Lista-02/Lista-02-java/src/Livro.java
@@ -0,0 +1,29 @@
+public class Livro
+{
+
+    String nome;
+    String autor;
+    int pag;
+
+    public Livro(String nome, String autor, int pag)
+    {
+        this.nome = nome;
+        this.autor = autor;
+        this.pag = pag;
+    }
+
+   public void abrirLivro()
+   {
+
+       System.out.println("Livro " + nome + " foi aberto!!");
+
+   }
+
+    public void lerPaginas()
+    {
+        for (int i = 1; i <=this.pag; i++)
+       {
+           System.out.println("Leu a página: " + i);
+        }
+    }
+}
---
+++ Aula 03/Lista-02/Lista-02-java/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+
+        Livro livro01 = new Livro("Pequeno principe", "Jorge", 210);
+
+        livro01.abrirLivro();
+
+        livro01.lerPaginas();
+    }
+}
+
